d9d0424d3ad20b3e30160572aaf5a91eaf772d0b
suraphop <mic@admin.com>
2023-07-25 11:11:57 +0700

    first commit

---
+++ jenkins/build/src/App.tsx
@@ -0,0 +1,23 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import FileUpload from "./components/FileUpload";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div className="bg-success p-2 text-white">
+        <h4>TYRAROND FILE UPLOAD</h4>
+      </div>
+      <div className="container" style={{ width: "500px" }}>
+        <div>
+          <div className="my-3">
+            <h4>เลือกไฟล์ tyrarond csv เท่านั้น</h4>
+          </div>
+          <FileUpload />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default App;
