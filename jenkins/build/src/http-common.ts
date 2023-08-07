7beeb55a8ab362df27fe4ed4758daf9f57bcbc64
Suraphop <62558689+Suraphop@users.noreply.github.com>
2023-08-07 15:46:45 +0700

    Update http-common.ts

--- jenkins/build/src/http-common.ts
+++ jenkins/build/src/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://10.128.16.210:8000/",
+  baseURL: "http://10.128.16.210:10000/",
   headers: {
     "Content-type": "application/json",
   },
